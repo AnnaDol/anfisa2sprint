3ae1418cc1d01f81a8000b5a04e69b0a7df33081
Anna Dolina <anna@dolina1998.ru>
2025-03-16 22:02:51 +0300

    change some file

--- anfisa_for_friends/core/models.py
+++ anfisa_for_friends/core/models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 class PublishedModel(models.Model):
     """Абстрактная модель. Добвляет флаг is_published."""
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField('Опубликовано', default=True)
+    title = models.CharField(
+        'Название',
+        max_length=256,
+        help_text='Уникальное название, не более 256 символов'
+        )
 
     class Meta:
         abstract = True
