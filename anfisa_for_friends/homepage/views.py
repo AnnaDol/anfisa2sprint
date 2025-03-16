15b25838672835e4c4464384726a2608c292dac9
Anna Dolina <anna@dolina1998.ru>
2025-03-16 22:59:12 +0300

    update file

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -6,8 +6,13 @@ from ice_cream.models import IceCream
 def index(request):
     template = 'homepage/index.html'
     ice_cream_list = IceCream.objects.values(
-        'id', 'title'
-        ).filter(is_on_main=True).exclude(is_published=False)
+        'id', 'title', 'price', 'description'
+        ).filter(
+            is_on_main=True,
+            is_published=True,
+            category__is_published=True
+        )
+    
     context = {
         'ice_cream_list': ice_cream_list,
     }
